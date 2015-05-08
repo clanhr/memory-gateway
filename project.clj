55fc986faca044364536289bbb4f0edeab292195
Pedro Santos <donbonifacio@gmail.com>
2015-05-08 11:51:41 +0100

    Add explicit clojure dep

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Simple in memory datastore for unit tests"
   :url "https://github.com/clanhr/memory-gateway"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"})
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-beta2"]])
