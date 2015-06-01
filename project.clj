c53d3520c800eed130d805125f0a58f3d60bf6f4
Pedro Santos <donbonifacio@gmail.com>
2015-06-01 12:55:28 +0100

    Add async interface
    Add core.async interface. Implemented core functions as core.async
    channels. At this moment this functions just act lik a proxy and wrap
    the core ones in a go context (go returns a channel with the result
    from the last expression).

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject clanhr/memory-gateway "0.6.0"
+(defproject clanhr/memory-gateway "0.7.0"
   :description "Simple in memory datastore for unit tests"
   :url "https://github.com/clanhr/memory-gateway"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta3"]])
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
---
+++ src/clanhr/memory_gateway/async.clj
@@ -0,0 +1,20 @@
+(ns clanhr.memory-gateway.async
+  "Memory async gateway utilities"
+  (:require [clanhr.memory-gateway.core :as core]
+            [clojure.core.async :refer [go]]))
+
+(defn save!
+  "Returns a channel with the result"
+  [& args]
+  (go (apply core/save! args)))
+
+(defn get-model
+  "Returns a channel with the result"
+  [& args]
+  (go (apply core/get-model args)))
+
+(defn paginated-query
+  "Returns a channel with the result"
+  [& args]
+  (go (apply core/paginated-query args)))
+
